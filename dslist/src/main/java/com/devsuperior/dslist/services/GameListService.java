88700308e6649064c5a25492b0014191060dbc4b
Carlos Mendes <carlosmendesjmc@gmail.com>
2025-08-20 19:10:15 -0300

    Get lists

---
+++ dslist/src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -0,0 +1,31 @@
+package com.devsuperior.dslist.services;
+
+
+
+import com.devsuperior.dslist.dto.GameListDTO;
+import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.repositories.GameListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+
+@Service
+public class GameListService {
+
+    @Autowired
+    private GameListRepository gameListRepository;
+
+
+
+    public List<GameListDTO> findAll() {
+        List<GameList> result = gameListRepository.findAll();
+        List<GameListDTO> dto = result.stream().map(x -> new GameListDTO(x)).toList();
+        return dto;
+    }
+
+
+}
